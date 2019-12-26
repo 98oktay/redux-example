47898a9aec0a8153295bbf61b7bfc640b33b49e1
Oktay <98oktay@gmail.com>
2019-12-26 13:20:18 +0300

    Redux example filled

--- src/js/components/Listing.jsx
+++ src/js/components/Listing.jsx
@@ -5,12 +5,16 @@ import * as Icons from '@fortawesome/free-solid-svg-icons'
 export default class Listing extends React.Component {
 
     state = {
-        items: [
-            {first: "Oktay", last: "Başkuş", age: 30}
+        users: [
+            {first: "Oktay", last: "Başkuş", age: 30},
+            {first: "Veronica", last: "Joseph", age: 34},
+            {first: "Lindsay", last: "Holt", age: 25},
+            {first: "Morris", last: "Castaneda", age: 35}
         ]
     };
 
     render() {
+        const users = this.state.users;
         return <><h4>Listing</h4>
             <div className="card border-success">
                 <table className="table table-striped table-borderless table-sm">
@@ -24,7 +28,8 @@ export default class Listing extends React.Component {
                     </tr>
                     </thead>
                     <tbody>
-                    {this.state.items.map((item, key) =>
+                    {!users.length && <tr><td colSpan={5} className="text-center">Empty list</td></tr>}
+                    {users.map((item, key) =>
                         <tr key={key}>
                             <th scope="row">{key}</th>
                             <td>{item.first}</td>
